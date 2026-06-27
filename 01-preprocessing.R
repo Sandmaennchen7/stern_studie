2bf1363c2528917e9b2bb30931152ab5b8624c75
h s <Hannah Stehle>
2026-06-27 20:03:33 +0200

    Add exclusion of under 18 and sound not played and video not played and writing assignment empty

--- 01-preprocessing.R
+++ 01-preprocessing.R
@@ -9,7 +9,8 @@ library(rio)
 
 ##read in data ========================
 raw_data_d <-read.csv('Rep01_demographics_anonymized.csv')
-raw_data <- read.csv('Rep01_anonymized.csv')
+raw_data <- read.csv("raw_data/Rep01_demographics_anonymized.csv")
+raw_data1 <- read.csv("raw_data/Rep01_anonymized.csv")
 
 
 ##create dataset removing all of the  inclusion/exclusion rules of our preregistration. See sections:
@@ -46,3 +47,38 @@ view(dfSummary(raw_data))
 
 View(data)
 view(dfSummary(data))
+
+view(raw_data1)
+View(dfSummary(raw_data1))
+raw_data1 %>%
+  select(writing_assignment) %>%
+  View()
+
+###M4 exclude participants under 18 
+names(raw_data)
+dat <- raw_data %>%
+  filter(age >= 18)
+view(dfSummary(dat))
+
+###look at the responses of the conscientious question: no one answered no, so I excluded no data, but many missing values, PreReg says that we only exculde participants who anwered no
+names(raw_data1)
+raw_data1 %>% count(conscientious)
+raw_data1 %>% count(video_problems)
+raw_data1 %>% count(video_played)
+raw_data1 %>% count(sound_on)
+
+###AP1 exclude participants who answered no to the question if the video played
+raw_data1 %>%
+  filter(video_played == 1)
+
+###AP1 exclude participants who answered no to the question if tone worked 
+raw_data1 %>%
+  filter(sound_on == 1)
+
+###AP1 exclude participants who didnt write anything to the writing assignment
+nrow(raw_data1)
+dat_clean <- raw_data1 %>%
+  filter(!is.na(writing_assignment),
+         trimws(writing_assignment) != "")
+nrow(dat_clean)
+view(dfSummary(dat_clean))
