e9eca3044c614fd99263e1eb6a5b13d3c637ab15
Chiara Wackermann <chiara.wackermann@gmx.de>
2026-06-30 03:02:34 +0200

    Datenbennenung nochmal überarbeitet - offene Fragen + Entwurf für Ausreißerselektion

--- 01-preprocessing.R
+++ 01-preprocessing.R
@@ -8,8 +8,7 @@ library(rio)
 
 
 ##read in data ========================
-raw_data_d <-read.csv('Rep01_demographics_anonymized.csv')
-raw_data <- read.csv("raw_data/Rep01_demographics_anonymized.csv")
+raw_demo <- read.csv("raw_data/Rep01_demographics_anonymized.csv")
 raw_data1 <- read.csv("raw_data/Rep01_anonymized.csv")
 
 
@@ -31,22 +30,38 @@ raw_data1 <- read.csv("raw_data/Rep01_anonymized.csv")
   #“AP2 
 
 ###preprocessing=======================
-dat <-raw_data %>%
-  #outlier <- df(..(mean(Number.Strengths)- Number.Strengths)^2 > 3)
-  #excl <- df(..Number.Strengths==0)
-  #mutate ()-> new column where 0 -> unproblematic data, 1 -> outlier, 2-> where numberofstrengths are =0
-  filter(SelectOut==0)
+
+dat <- raw_data1 %>%
+  mutate(
+    # classification column
+    SelectOut = case_when(
+      # Condition for Category 2: Number.Strengths is exactly 0
+      Number.Strengths == 0 ~ 2,
+      
+      # Condition for Category 1: Outlier in either Strengths OR Weaknesses (> 3 SD)
+      abs(Number.Strengths - mean(Number.Strengths, na.rm = TRUE)) > (3 * sd(Number.Strengths, na.rm = TRUE)) | 
+        abs(Number.Weaknesses - mean(Number.Weaknesses, na.rm = TRUE)) > (3 * sd(Number.Weaknesses, na.rm = TRUE)) ~ 1,
+      
+      # Category 0: Everything else (Unproblematic)
+      TRUE ~ 0
+    )
+  ) %>%
+  
+  # Filter
+  filter(SelectOut == 0)
+
+# Export the processed data
 export(dat, file="processed_data/data.csv") #save data in file
 
 
 
 ###viewdata==
-table(raw_data$SelectOut)
-View(raw_data)
-view(dfSummary(raw_data))
+table(raw_demo$SelectOut)
+View(raw_demo)
+view(dfSummary(raw_demo))
 
-View(data)
-view(dfSummary(data))
+View(dat)
+view(dfSummary(dat)) ##was genau ist das? ~Chiara
 
 view(raw_data1)
 View(dfSummary(raw_data1))
@@ -55,10 +70,10 @@ raw_data1 %>%
   View()
 
 ###M4 exclude participants under 18 
-names(raw_data)
-dat <- raw_data %>%
+names(raw_demo)
+demo <- raw_demo %>%
   filter(age >= 18)
-view(dfSummary(dat))
+view(dfSummary(demo))
 
 ###look at the responses of the conscientious question: no one answered no, so I excluded no data, but many missing values, PreReg says that we only exculde participants who anwered no
 names(raw_data1)
